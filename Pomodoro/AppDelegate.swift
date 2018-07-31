581c383823a9c5db1f38acf2c8cd2fc4390d8ffd
Kirk Northrop <kirk.northrop@novastonemedia.com>
2018-07-31 16:34:49 +0100

    Initial Commit

---
+++ Pomodoro/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Pomodoro
+//
+//  Created by Kirk Northrop on 31/07/2018.
+//  Copyright © 2018 Novastone Media. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
