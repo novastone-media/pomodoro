976da3845e1906ddb5dac3b7867ea0ac1eae2fac
Kirk Northrop <kirk.northrop@novastonemedia.com>
2018-11-06 14:32:19 +0000

    Add rest of app

--- Pomodoro/AppDelegate.swift
+++ Pomodoro/AppDelegate.swift
@@ -7,20 +7,102 @@
 //
 
 import Cocoa
+import AVFoundation
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
-
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+    let statusItem = NSStatusBar.system.statusItem(withLength:80)
+    let calendar = Calendar.current
+    var audioPlayer:AVAudioPlayer!
+    var enableSounds:NSMenuItem!
+    
+    var soundOn = true
+    
+    func makeStartNoise() {
+        makeANoise(filename: "start")
+    }
+    
+    func makeEndNoise() {
+        makeANoise(filename: "end")
+    }
+    
+    @objc func toggleSounds() {
+        soundOn = !soundOn
+        if (soundOn) {
+            enableSounds.state = NSControl.StateValue.on
+        } else {
+            enableSounds.state = NSControl.StateValue.off
+        }
+    }
+    
+    func makeANoise(filename: String) {
+        if (soundOn) {
+            let bundle = Bundle.main
+            let audioFilePath = bundle.path(forResource:filename, ofType: "wav")
+            
+            if audioFilePath != nil {
+                let audioFileUrl = NSURL.fileURL(withPath:audioFilePath!)
+                
+                do {
+                    try audioPlayer = AVAudioPlayer(contentsOf: audioFileUrl)
+                    audioPlayer.play()
+                } catch {
+                }
+            }
+        }
+    }
+    
+    @objc func updateCounters() {
+        let date = Date()
+        let minutes = calendar.component(.minute, from: date)
+        let seconds = calendar.component(.second, from: date)
+        let secondsLeft = 59 - seconds
+        let count = minutes % 30
+        
+        if (count > 24 && count < 30) {
+            let minutesLeft = 29 - count
+            updateMenuBarCounter(minutesLeft: minutesLeft, secondsLeft: secondsLeft, isBreak: true)
+        } else {
+            let minutesLeft = 24 - count
+            updateMenuBarCounter(minutesLeft: minutesLeft, secondsLeft: secondsLeft, isBreak: false)
+        }
     }
 
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+    func updateMenuBarCounter(minutesLeft: Int, secondsLeft: Int, isBreak: Bool) {
+        statusItem.button?.appearsDisabled = !isBreak
+
+        if (isBreak) {
+            statusItem.button?.title =  String(format: "☕️ %02d:%02d", minutesLeft, secondsLeft)
+            if (isBreak && minutesLeft == 4 && secondsLeft == 59) {
+                 makeEndNoise()
+            }
+        } else {
+            statusItem.button?.title =  String(format: "🍅 %02d:%02d", minutesLeft, secondsLeft)
+            if (minutesLeft == 24 && secondsLeft == 59) {
+                makeStartNoise()
+            }
+        }
+    }
+    
+    func constructMenu() {
+        let menu = NSMenu()
+        
+        enableSounds = NSMenuItem(title: "Sounds", action: #selector(AppDelegate.toggleSounds), keyEquivalent: "s")
+        menu.addItem(enableSounds)
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(NSMenuItem(title: "Quit", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
+        
+        enableSounds.state = NSControl.StateValue.on
+        statusItem.menu = menu
     }
 
-
+    func applicationDidFinishLaunching(_ aNotification: Notification) {      
+        let timer = Timer.init(timeInterval: 1, target: self,   selector: (#selector(updateCounters)), userInfo: nil, repeats: true)
+        
+        updateCounters()
+        constructMenu()
+        
+        RunLoop.main.add(timer, forMode: RunLoop.Mode.common)
+    }
 }
-
---
+++ README.md
@@ -0,0 +1,12 @@
+# macOS Pomodoro Timer
+
+Sounds from [Dusty Room](http://dustyroom.com/free-casual-game-sounds/) and licenced under [CC0](https://creativecommons.org/share-your-work/public-domain/cc0/)
+Graphics Copyright 2018 Twitter, Inc and other contributors and licensed under [CC-BY 4.0](https://creativecommons.org/licenses/by/4.0/)
+
+Copyright © Novastone Media Limited 2018
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.
